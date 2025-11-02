15ed1726ca38d36184eb0e3eba42728d613ebf50
Yoshiki Shibukawa <yoshiki@shibu.jp>
2025-11-02 23:44:24 +0900

    remove unused code (#92)

--- generate.go
+++
@@ -1,4 +0,0 @@
-//go:generate go run ./cmd/snapsql generate --config=examples/kanban/snapsql.yaml
-//go:generate go run ./cmd/snapsql generate --config=examples/kanban/snapsql_pg.yaml
-
-package snapsql
